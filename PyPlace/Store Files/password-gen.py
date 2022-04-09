38ba0467283e8ac03d3b36313cf281417ff586bc
dante-nl <61943433+dante-nl@users.noreply.github.com>
2022-04-09 16:26:29 +0200

    Made it more random! :D

--- PyPlace/Store Files/password-gen.py
+++ PyPlace/Store Files/password-gen.py
@@ -5,11 +5,12 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_letters= int(input("How many letters would you like in your password?\n") or 5) 
+nr_symbols = int(input(f"How many symbols would you like?\n") or 5)
+nr_numbers = int(input(f"How many numbers would you like?\n") or 5)
 
 letter_password = None
+password_list = []
 
 for letter in range(1, nr_letters + 1):
 	random_letter = random.choice(letters)
@@ -18,14 +19,19 @@ for letter in range(1, nr_letters + 1):
 	else:
 		letter_password = random_letter
 
-number_password = None
+for letter in letter_password:
+	password_list.append(letter)
 
+number_password = None
 for number in range(1, nr_numbers + 1):
-	random_number = random.choice(numbers)
+	random_number = str(random.choice(numbers))
 	if number_password != None:
 		number_password = random_number + number_password
 	else:
 		number_password = random_number
+
+for number in number_password:
+	password_list.append(number)
 
 symbol_password = None
 
@@ -36,13 +42,10 @@ for number in range(1, nr_symbols + 1):
 	else:
 		symbol_password = random_symbol
 
+for symbol in symbol_password:
+	password_list.append(symbol)
 
-password = letter_password + number_password + symbol_password
+random.shuffle(password_list)
+
+password = "".join(password_list)
 print(f"The generated password is: {password}")
-
-def Convert(string):
-	list1 = []
-	list1[:0] = string
-	return list1
-
-print(random.shuffle(Convert(password)))
